58113c3b1bdbd77ad855930cf676edc71075ab7c
kethiaumumararungu-tech <kethiaumumararungu@gmail.com>
2026-09-20 07:01:33 -0700

    Add files via upload

---
+++ queries.sql/queris.sql.sql
@@ -0,0 +1,14 @@
+-- =========================================================
+-- JOIN QUERY 1
+-- List every order with customer information
+-- =========================================================
+
+SELECT
+    o.order_id,
+    c.customer_name,
+    c.city,
+    o.order_date
+FROM orders o
+INNER JOIN customers c
+    ON o.customer_id = c.customer_id
+ORDER BY o.order_date;
